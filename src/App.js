c9a963b994fad0d9bfee00bd7903ae9b8eb0192d
Alejandro Mazas de Lizana <alejandro.mazasdelizana@gmail.com>
2023-05-25 22:07:03 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          working React
         </a>
       </header>
     </div>
